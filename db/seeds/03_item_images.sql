2743706e8eb90f4eb4901bd85a37ec0b18012cce
Nik Hixson <nikilobe@gmail.com>
2025-01-10 18:08:28 -0800

    all my test stuff

--- db/seeds/03_item_images.sql
+++ db/seeds/03_item_images.sql
@@ -2,4 +2,12 @@
 
 INSERT INTO item_images (item_id, image_url) VALUES
 (1, 'https://i.etsystatic.com/25519128/r/il/4fdfb6/5751881320/il_794xN.5751881320_t0d7.jpg'),
-(2, 'https://i.etsystatic.com/7192509/r/il/4672ef/2135170489/il_794xN.2135170489_k8db.jpg');
+(2, 'https://i.etsystatic.com/7192509/r/il/4672ef/2135170489/il_794xN.2135170489_k8db.jpg'),
+(3, 'https://i.etsystatic.com/12345678/r/il/abcd1234/1234567890/il_794xN.1234567890_abcd.jpg'),
+(4, 'https://i.etsystatic.com/87654321/r/il/efgh5678/0987654321/il_794xN.0987654321_efgh.jpg'),
+(5, 'https://i.etsystatic.com/23456789/r/il/ijkl9012/3456789012/il_794xN.3456789012_ijkl.jpg'),
+(6, 'https://i.etsystatic.com/34567890/r/il/mnop3456/4567890123/il_794xN.4567890123_mnop.jpg'),
+(7, 'https://i.etsystatic.com/45678901/r/il/qrst7890/5678901234/il_794xN.5678901234_qrst.jpg'),
+(8, 'https://i.etsystatic.com/67890123/r/il/uvwx5678/6789012345/il_794xN.6789012345_uvwx.jpg'),
+(9, 'https://i.etsystatic.com/78901234/r/il/yzab6789/7890123456/il_794xN.7890123456_yzab.jpg'),
+(10, 'https://i.etsystatic.com/89012345/r/il/cdef7890/8901234567/il_794xN.8901234567_cdef.jpg');
