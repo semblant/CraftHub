04d6b41d9cc765e598a98c9642dc6f2d6473d071
semblant <robynhumphreys7_4@hotmail.com>
2025-01-10 18:08:39 -0700

    Change to mobile first styling, Add on click event listener to Nav bar menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,27 @@
 // Client facing scripts here
+
+
+$(document).ready(function() {
+  console.log('jQuery is working!')
+  let isSubmenuVisible = false; // Track submenu visibility
+  // Add event listener for menu item on mobile
+  $('.fa-solid.fa-bars').on('click', function () {
+    console.log('Menu item was clicked!')
+
+    if (isSubmenuVisible) {
+      $('.menuItems').fadeOut(); // Hide submenu
+    } else {
+      $('.menuItems').fadeIn(); // Show submenu
+    }
+    isSubmenuVisible = !isSubmenuVisible; // Toggle state
+  });
+
+  // Close menu if click is outside the menu
+  $(document).on('click', function (e) {
+    console.log('Clicked outside:', e.target);
+    if (!$(e.target).closest('.fa-bars, .menuItems').length) {
+      $('.menuItems').fadeOut();
+      isSubmenuVisible = false;
+    }
+  });
+});
