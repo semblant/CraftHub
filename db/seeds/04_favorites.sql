067c45989f05faf6c65a62b450e646c64f32e8bd
Nik Hixson <nikilobe@gmail.com>
2025-01-11 11:51:15 -0800

    Add is_active column to favorites table and update seed data

--- db/seeds/04_favorites.sql
+++ db/seeds/04_favorites.sql
@@ -1,5 +1,5 @@
 -- Favorites table seeds
 
-INSERT INTO favorites (user_id, item_id) VALUES
-(2, 1),
-(2, 2);
+INSERT INTO favorites (user_id, item_id, is_active) VALUES
+(2, 1, false),
+(2, 2, false);
