92cc42e4922c7bcdfda25f6f7012f11ac405acec
Nik Hixson <nikilobe@gmail.com>
2025-01-13 19:02:54 -0800

    Update item image URLs in seed data for improved quality

--- db/seeds/03_item_images.sql
+++ db/seeds/03_item_images.sql
@@ -6,8 +6,8 @@ INSERT INTO item_images (item_id, image_url) VALUES
 (3, 'https://th.bing.com/th/id/OIG2.0YvyAT9Rnk8FPO1XBDp_?w=1024&h=1024&rs=1&pid=ImgDetMain'),
 (4, 'https://th.bing.com/th/id/OIG3.2A8Lt8FVHKeGY1JQw_uL?pid=ImgGn'),
 (5, 'https://th.bing.com/th/id/OIG3.JvQgxfYWR7_9ydhhUSo1?w=1024&h=1024&rs=1&pid=ImgDetMain'),
-(6, 'https://i.etsystatic.com/34567890/r/il/mnop3456/4567890123/il_794xN.4567890123_mnop.jpg'),
-(7, 'https://i.etsystatic.com/45678901/r/il/qrst7890/5678901234/il_794xN.5678901234_qrst.jpg'),
-(8, 'https://i.etsystatic.com/67890123/r/il/uvwx5678/6789012345/il_794xN.6789012345_uvwx.jpg'),
-(9, 'https://i.etsystatic.com/78901234/r/il/yzab6789/7890123456/il_794xN.7890123456_yzab.jpg'),
-(10, 'https://i.etsystatic.com/89012345/r/il/cdef7890/8901234567/il_794xN.8901234567_cdef.jpg');
+(6, 'https://th.bing.com/th/id/OIG2.pK3vSjn4NiDJJ1_H6Zdm?w=1024&h=1024&rs=1&pid=ImgDetMain'),
+(7, 'https://th.bing.com/th/id/OIG2.5XAyYGcdJ_0G6rtz.HfO?pid=ImgGn'),
+(8, 'https://th.bing.com/th/id/OIG1.x4zO1CQ0NOo4LCfAs3.i?w=1024&h=1024&rs=1&pid=ImgDetMain'),
+(9, 'https://th.bing.com/th/id/OIG4.SrqQPjlsyDPGeOCHlc8A?w=1024&h=1024&rs=1&pid=ImgDetMain'),
+(10, 'https://th.bing.com/th/id/OIG2..iIYIeuMgEFIMFTezh4Z?w=1024&h=1024&rs=1&pid=ImgDetMain');
