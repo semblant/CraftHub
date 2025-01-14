a96e8207c43c1b17d7b0c41de2ccad6263161637
Nik Hixson <nikilobe@gmail.com>
2025-01-14 13:45:54 -0800

    Return item linking to details page

--- public/scripts/filters.js
+++ public/scripts/filters.js
@@ -12,32 +12,33 @@ $(() => {
           const $item = $(`
           <div class="product">
             <div class="product-header">
-              <span class="product-title">${item.title}</span>
+              <a class="product-title" href="/detailed-item/${item.id}">${item.title}</a>
               <span class="product-price">$${item.price}</span>
             </div>
             <img src="${item.image_url}" alt="${item.title}">
             <div class="product-footer">
               <button class="chat-button">💬</button>
             </div>
-            <i class="favorite-icon fa-solid fa-heart" data-item-id="${item.id}" style="color: #ff0000;"></i>
+            <i class="favorite-icon fa-solid fa-heart" data-item-id="${item.id}" style="color: ${item.is_favorited ? 'red' : '#414141'};"></i>
           </div>
         `);
           $itemsList.append($item);
         }
+      })
+      .fail(() => {
+        alert('Failed to fetch items. Please try again later.');
       });
-
-    // Filter items by price
-    $('.filter-button-price').click(() => {
-      const minPrice = prompt("Enter minimum price:");
-      const maxPrice = prompt("Enter maximum price:");
-      fetchItems(`/api/items/filter/price?minPrice=${minPrice}&maxPrice=${maxPrice}`);
-    });
-
-    // Filter favorited items
-    $('.filter-button-favorites').click(() => {
-      fetchItems('/api/items/favorites');
-    });
   };
 
-  fetchItems('/api/items'); // Initial fetch to load all items
+  // Filter items by price
+  $('.filter-button-price').click(() => {
+    const minPrice = prompt("Enter minimum price:");
+    const maxPrice = prompt("Enter maximum price:");
+    fetchItems(`/api/items/filter/price?minPrice=${minPrice}&maxPrice=${maxPrice}`);
+  });
+
+  // Filter favorited items
+  $('.filter-button-favorites').click(() => {
+    fetchItems('/api/items/favorites');
+  });
 });
