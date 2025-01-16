6a4af205ac72b267940937bb1082ab1c477bb66f
Nik Hixson <nikilobe@gmail.com>
2025-01-16 09:07:54 -0800

    Enhance styling and structure in home view; add mixins and box shadows in SCSS; update item descriptions in SQL seed

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -3,11 +3,11 @@
 INSERT INTO items (title, description, price, category_id, status, user_id) VALUES
 ('Handmade Scarf', 'A beautiful handmade scarf.', 25.00, 1, 'Available', 1),
 ('Knitted Hat', 'A duck hat.', 15.00, 2, 'Available', 1),
-('Handmade Wooden Bowl', 'A rustic handmade wooden bowl.', 25.00, 1, 'Available', 1),
+('Cute Wood Bowl', 'A rustic handmade wooden bowl.', 25.00, 1, 'Available', 1),
 ('Ceramic Vase', 'A unique handmade ceramic vase.', 45.00, 2, 'Available', 1),
 ('Leather Wallet', 'A stylish handmade leather wallet.', 50.00, 1, 'Available', 1),
 ('Beaded Necklace', 'A colorful handmade beaded necklace.', 20.00, 2, 'Available', 1),
-('Handwoven Basket', 'A sturdy handmade handwoven basket.', 30.00, 1, 'Available', 1),
-('Handmade Candle', 'A scented handmade candle.', 15.00, 2, 'Available', 1),
+('Woven Basket', 'A sturdy handmade handwoven basket.', 30.00, 1, 'Available', 1),
+('Scented Candle', 'A scented handmade candle.', 15.00, 2, 'Available', 1),
 ('Handmade Soap', 'A natural handmade soap.', 10.00, 1, 'Available', 1),
 ('Handmade Quilt', 'A cozy handmade quilt.', 100.00, 2, 'Available', 1);
